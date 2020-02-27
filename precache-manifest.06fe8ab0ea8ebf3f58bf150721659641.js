729224fceed47d0dfa8e98ae5b5230f3a394bee7
i1-kmv <i1-kmv@mail.ru>
2020-02-27 16:59:02 +0200

    Updates

---
+++ precache-manifest.06fe8ab0ea8ebf3f58bf150721659641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d783af9b6b5d513c52253a934973329",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "22671ccb55fd85169025",
+    "url": "/react-portfolio/static/css/main.0c9f4948.chunk.css"
+  },
+  {
+    "revision": "dae02090e3910b0cf7c1",
+    "url": "/react-portfolio/static/js/2.3f015424.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-portfolio/static/js/2.3f015424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22671ccb55fd85169025",
+    "url": "/react-portfolio/static/js/main.cbd69c6d.chunk.js"
+  },
+  {
+    "revision": "d418517a936e30f0d2bc",
+    "url": "/react-portfolio/static/js/runtime-main.cfaa447c.js"
+  }
+]);
