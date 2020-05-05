30c73897d45b46d41588f9533aa782657a2b8489
i1-kmv <i1-kmv@mail.ru>
2020-05-05 18:31:32 +0200

    2-3 этап ready

--- src/conponents/Main/Main.jsx
+++ src/conponents/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Main.module.css'
 
+
 const Main = () => {
     return (
         <div className={styles.main}>
--- src/conponents/Works/WorksItem/WorksItem.jsx
+++ src/conponents/Works/WorksItem/WorksItem.jsx
@@ -3,16 +3,10 @@ import styles from './WorksItem.module.css'
 import Button from "../../Button/Button";
 
 const WorksItem = (props) => {
+
     return (
         <div className={styles.work}>
-            <div className={styles.img}>
-                <img src="#" alt="картинка"/>
-            </div>
-            <Button name={props.name}/>
-            <div className={styles.description}>
-                <span>Название проекта</span>
-                <span>Краткое описание</span>
-            </div>
+            <Button className={styles.button} name={props.name} />
         </div>
     );
 }
