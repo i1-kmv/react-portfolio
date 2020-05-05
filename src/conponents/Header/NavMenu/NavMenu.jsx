8078f48b0e74cd5d8dedf136ae8101f54f045f20
i1-kmv <i1-kmv@mail.ru>
2020-05-05 12:41:51 +0200

    Настройка адаптива

--- src/conponents/Header/NavMenu/NavMenu.jsx
+++ src/conponents/Header/NavMenu/NavMenu.jsx
@@ -4,10 +4,10 @@ import styles from './NavMenu.module.css'
 const NavMenu = () => {
     return (
                 <div className={styles.nav}>
-                    <a href="" className={styles.nav__link}>Главная</a>
-                    <a href="" className={styles.nav__link}>Скиллы</a>
-                    <a href="" className={styles.nav__link}>Проекты </a>
-                    <a href="" className={styles.nav__link}>Контакты</a>
+                    <a href="" className={styles.nav__link}>Main</a>
+                    <a href="" className={styles.nav__link}>Skills</a>
+                    <a href="" className={styles.nav__link}>Works</a>
+                    <a href="" className={styles.nav__link}>Contacts</a>
                 </div>
     );
 }
