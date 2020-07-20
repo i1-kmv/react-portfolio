ab4668fae1f29898f02ea34658b6ee36aa88d047
i1-kmv <i1-kmv@mail.ru>
2020-07-20 20:23:06 +0200

    +2projects (Art, window shop)

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,12 @@ import counterImg from "../../image/2.png";
 import walletImg from "../../image/3.png";
 import repairImg from "../../image/4.png";
 import skinaliImg from "../../image/5.png";
+import blogImg from "../../image/6.png";
+import gotImg from '../../image/7.png';
+import artImg from '../../image/8.png';
+import windowsImg from '../../image/9.png';
+
+
 import Fade from 'react-reveal/Fade';
 
 
@@ -16,6 +22,10 @@ const Projects = (props) => {
         {name: 'Expense calculator', link: 'https://i1-kmv.github.io/Present-calc/', style: {backgroundImage: `url(${walletImg})`}},
         {name: 'Repair', link: 'https://i1-kmv.github.io/repair-gh/', style: {backgroundImage: `url(${repairImg})`}},
         {name: 'Skinali', link: 'https://i1-kmv.github.io/skinali-/',   style: {backgroundImage: `url(${skinaliImg})`}},
+        {name: 'React blog page', link: 'https://i1-kmv.github.io/React-Blog/',   style: {backgroundImage: `url(${blogImg})`}},
+        {name: 'Work with GOT Api', link: 'https://i1-kmv.github.io/React-GotApp/',   style: {backgroundImage: `url(${gotImg})`}},
+        {name: 'ART', link: 'https://i1-kmv.github.io/Art/',   style: {backgroundImage: `url(${artImg})`}},
+        {name: 'Window shop', link: 'https://i1-kmv.github.io/windowMarketIrvas/',   style: {backgroundImage: `url(${windowsImg})`}},
     ]
 
 
